802630dd235379e7e68932380123e816806b6558
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-05-05 15:03:50 -0300

    feat: dashboard page and layout

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,32 @@
+import ApiDashboard from '@/components/ApiDashboard'
+import RequestApiKey from '@/components/RequestApiKey'
+import { authOptions } from '@/lib/auth'
+import { db } from '@/lib/db'
+import type { Metadata } from 'next'
+import { getServerSession } from 'next-auth'
+import { notFound } from 'next/navigation'
 
-const page = () => {
+export const metadata: Metadata = {
+    title: 'Similarity API | Dashboard',
+    description: 'Free & open-source text similarity API'
+}
+
+const page = async () => {
+  const user = await getServerSession(authOptions)
+
+  if(!user) return notFound()
+
+  const apiKey = await db.apiKey.findFirst({
+    where: { 
+        userId: user.user.id,
+        enabled: true
+    }
+  })
+
   return (
-    <div>page</div>
+    <div className="max-w-7xl mx-auto mt-16">
+        {apiKey ? <ApiDashboard /> : <RequestApiKey />}
+    </div>
   )
 }
 
